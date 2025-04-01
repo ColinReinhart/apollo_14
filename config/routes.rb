e3fa408b4d2a8bf8a1af3cb6348683bb072b15bf
Colin Reinhart <colinbreinhart@gmail.com>
2025-04-01 14:24:41 -0600

    Completed Extension

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/astronauts", to: "astronauts#index"
+  get "/astronauts/:id", to: "astronauts#show"
 end
---
+++ extensions/x86_64-darwin-24/3.2.0/pg-1.5.3/pg/postgresql_lib_path.rb
@@ -0,0 +1,3 @@
+module PG
+	POSTGRESQL_LIB_PATH = "/Users/colinreinhart/anaconda3/lib"
+end
